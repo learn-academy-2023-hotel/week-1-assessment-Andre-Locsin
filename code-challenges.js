9dbe282247cea3eafc76594441755a3ce5225037
LEARN Academy <learnacademy@MacBook-Air.local>
2023-10-16 08:35:13 -0700

    first commit

--- code-challenges.js
+++ code-challenges.js
@@ -10,36 +10,84 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a conditional statement that evaluates two strings and determines which of the strings has more characters. Use the two sets of test variables provided.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+// // // Expected outcome: "banana"
+// if(fruit1.length > fruit2.length){
+//     console.log(fruit1)
+// }else if(fruit2.length > fruit1.length){
+//     console.log(fruit2)
+// }
+// conditional statement to compare which string is longer based on index
+// first console.log outputs fruit1 if it is longer
+// second console.log outputs fruit2 if it is longer
+// compares the index of the 2 strings
 
 // Set two:
 // const fruit1 = "cherry"
 // const fruit2 = "kiwi"
-// Expected outcome: "cherry"
+
+// const fruit1 = "cherry"
+// const fruit2 = "kiwi"
+// // Expected outcome: "cherry"
+// if(fruit1.length > fruit2.length){
+//     console.log(fruit1)
+// }else if(fruit2.length > fruit1.length){
+//     console.log(fruit2)
+// }
 
 // Pseudo code:
+// determine what data type we are working with
+// set a condition which compares data types which are 2 strings
+// (.length) is used to count the number of index there is in our string
+// we are using "if" statements as our conditional statements to get a bolean equivalent from the 2 items being compared
+// (console.log) is the method we are using to output our statement which is attatched to a data type that is a variable so we can replace true or false with said data type to get the content that is assigned to the variable given to us
+// else if is for the other variable that we have if the 2nd variables index is larger
+
 
 
 // --------------------1) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+ const padresWorldSeriesRuns = (padres1984WorldSeriesRuns.length) + (padres1998WorldSeriesRuns.legnth)
+ console.log(padresWorldSeriesRuns)
+
 // Expected output: 9
 
 // Pseudo code:
 
 // --------------------2) Create the code that will reverse the letters of a string using the test variable provided below.
 
-const currentCohort = "Golf 2023"
-// Expected output: "3202 floG"
-
+// const currentCohort = "Golf 2023"
+// // Expected output: "3202 floG"
+// const myArray = (currentCohort.split('').reverse().join(""))
+// console.log(myArray)
 // Pseudo code:
+// (myArray)- set our data type
+//(.split) is to split the string into individual strings in order to reverse our array because you cannot reverse a string
+// (.reverse) to reverse our array 
+//(.join) to join all our index in the array to turn into a string
+
 
 // --------------------3) Create the code that will log only the odd numbers from the array using the test variable provided below.
 
-const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
-// Expected output: 13 5 -5 27
+// const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
+// // Expected output: 13 5 -5 27
+// for(let i = 0; i < stockExchange.length; i++){
+//     if(stockExchange[i] % 2 !== 0)
+//     console.log(stockExchange[i])
+// }
 
 // Pseudo code:
+// stockExchange is our given array
+
+// (let i=0) =>starts us off at the 0 index
+// (i < stockExchange.length) => compares each index of the array due to ().length)
+// (i++) => allowing us to go up by 1 after each comparison unstil the end of .length
+// we create a condition using (if) where we can compare data type from the array
+// (stockExchange[i] % 2 !== 0) 
+// [i] => is to compare each value of the array
+// % is to attain the remainder after it is compared to a number which is 2 in our case to equate all even numbers into 0
+// !== 0 => so that every number that is not equal to 0 will be recycled back into the array
+// (console.log(stockExchange[i])) => is to log the value of the index due to the [] command and i is allowing it to select from the array
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,49 +7,49 @@
 // There is no need to change any of the code.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+// const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 5
+// b) Verify and explain:it would input the number of index in the array and not the actual array due to the lack of []
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:10
+// b) Verify and explain:it will execute the .length and count the amount that is in the array without extracting the specific value because [] were not used 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: due to [4] it gives as the 4th index from the array and its value in our case is a string and the 4th index is "o" because when counting an array its start with the number "0" "1" so on and so forth
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 1
+// b) Verify and explain: because of the const 1 it will give us its value from console.log index = 1
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: error or undefined
+// b) Verify and explain: you cannot .toUppercase an array without converting it to an individual string then uppercasing it afterwards
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 28
+// b) Verify and explain: 
